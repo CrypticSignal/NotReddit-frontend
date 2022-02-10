1b875bbcc86e2bcd259d164861d4d772e4e2bbda
CrypticSignal <hshafiq@hotmail.co.uk>
2022-02-10 12:49:14 +0000

    Set a default logged in user

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import SingleArticle from "./components/SingleArticle";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState("jessjelly");
   return (
     <Container>
-      <NavBar />
+      <NavBar user={loggedInUser} />
       <Routes>
         <Route path="/" element={<Articles />}></Route>
         <Route path="/topics/:topic" element={<Articles />}></Route>
